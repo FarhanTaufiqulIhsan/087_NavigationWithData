a70573802f774d805849a1f8f931e953bca90315
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-11-14 20:29:10 +0700

    Add HalamanForm function and declare 3 variables

--- app/src/main/java/com/example/esjumbo/HalamanForm.kt
+++ app/src/main/java/com/example/esjumbo/HalamanForm.kt
@@ -1,2 +1,15 @@
 package com.example.esjumbo
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+
+@Composable
+fun HalamanForm(){
+    var namaTxt by rememberSaveable { mutableStateOf("") }
+    var alamatTxt by remember { mutableStateOf("") }
+    var teleponTxt by remember { mutableStateOf("") }
+}
