0b4877296e2eeafdd4ee9788a28329d7e2726128
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-11-14 21:16:39 +0700

    Add OutlineTextField with text "Nomor Telepon" in it

--- app/src/main/java/com/example/esjumbo/HalamanForm.kt
+++ app/src/main/java/com/example/esjumbo/HalamanForm.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -17,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -50,6 +52,12 @@ fun HalamanForm(){
             value = alamatTxt,
             onValueChange = {alamatTxt = it},
             label = { Text(stringResource(R.string.alamat))})
+
+        OutlinedTextField(
+            value = teleponTxt,
+            onValueChange = {teleponTxt = it},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text(stringResource(R.string.notelp))})
 
     }
 }
