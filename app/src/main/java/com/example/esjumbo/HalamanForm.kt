d182c1e2a4d880f170b4e32ededff402fff169e1
TC1087 <farhan.taufiqul.ft21@mail.umy.ac.id>
2023-11-14 20:46:20 +0700

    Add Column and make it in center and add text "Data Pelanggan" with its style

--- app/src/main/java/com/example/esjumbo/HalamanForm.kt
+++ app/src/main/java/com/example/esjumbo/HalamanForm.kt
@@ -1,15 +1,38 @@
 package com.example.esjumbo
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HalamanForm(){
     var namaTxt by rememberSaveable { mutableStateOf("") }
     var alamatTxt by remember { mutableStateOf("") }
     var teleponTxt by remember { mutableStateOf("") }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(text = "Data Pelanggan",
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
 }
